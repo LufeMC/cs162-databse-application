0e3a39bb580645862555f7a9ddd80944dbd4f457
Luis Fernando Cerqueira <fernando.luis953@gmail.com>
2023-03-06 02:33:03 +0530

    ficing ci and adding .env

--- config.py
+++ config.py
@@ -1,19 +1,24 @@
+from dotenv import load_dotenv
+from os import environ as env
 import os
 import pymysql
 import urllib
 pymysql.install_as_MySQLdb()
 
+load_dotenv()
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    db_user = os.environ.get('DATABASE_USERNAME')
-    db_pass = urllib.parse.quote(os.environ.get('DATABASE_PASSWORD'))
-    db_host = os.environ.get('DATABASE_HOST')
-    db_port = os.environ.get('DATABASE_PORT')
-    db_name = os.environ.get('DATABASE_NAME')
+    db_user = env['DATABASE_USERNAME']
+    db_pass = urllib.parse.quote(env['DATABASE_PASSWORD'])
+    db_host = env['DATABASE_HOST']
+    db_port = env['DATABASE_PORT']
+    db_name = env['DATABASE_NAME']
+    print(db_name, db_pass, db_host, db_port, db_user)
 
     SQLALCHEMY_DATABASE_URI = f"mysql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"\
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = env['SECRET_KEY']
